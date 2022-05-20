c9807967f22e0caaeb6b769b951c539776246282
NULL-header <poaoq.poaoq@gmail.com>
2022-05-21 06:29:05 +0900

    feat macro

--- register_assert_macro/src/lib.rs
+++ register_assert_macro/src/lib.rs
@@ -2,7 +2,27 @@ extern crate proc_macro;
 
 mod parse;
 
+use parse::Args;
 use proc_macro::TokenStream;
+use quote::quote;
 
-// #[proc_macro]
-// pub fn register_assert(args: TokenStream) -> TokenStream {}
+#[proc_macro]
+pub fn register_assert(args: TokenStream) -> TokenStream {
+    let args: Args = match syn::parse(args) {
+        Ok(a) => a,
+        Err(e) => {
+            return e.to_compile_error().into();
+        }
+    };
+    let parsable = args.get_parsable();
+    let error = args.get_error();
+    quote! {
+        type Assert = assert_parse::Assert<#parsable, #error>;
+
+        #[fixture]
+        fn assert() -> Assert {
+            make_assert()
+        }
+    }
+    .into()
+}
